20c52bd5c8bbaaee938dabb0a7bb02f08c6c5a4f
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2023-05-24 01:39:53 +0530

    new changes

--- src/component/Devices.js
+++ src/component/Devices.js
@@ -3,54 +3,52 @@ import SIdebar from "./SIdebar";
 import { Link } from "react-router-dom";
 export default function Devices() {
   return (
-    <div className="devices h-100vh">
-      <div className="d-flex">
-        <SIdebar />
-        <div className="devices_content d-flex flex-direction-column w-100">
-          <h1>Devices</h1>
-          <div className="devices_content_container">
-            <div className="devices_content_left">
-              <img
-                src="/devices_content_left_img.png"
-                alt=""
-                className="img-fluid"
-              />
-              <h2>Get Flax Card</h2>
-              <p>
-                Don’t have Flax Card
-                <br />
-                Purchase them here.
-              </p>
-              <a
-                href="https://caard.mini.store/"
-                target="blank"
-                className="devices-primary d-inline-block"
-              >
-                Get Card
-              </a>
-            </div>
-            <div className="devices_content_border"></div>
-            <div className="devices_content_right">
-              <img
-                src="/devices_content_right_img.png"
-                alt=""
-                className="img-fluid"
-              />
+    <div className="devices h-100vh d-flex">
+      <SIdebar />
+      <div className="devices_content d-flex flex-direction-column w-100">
+        <h1>Devices</h1>
+        <div className="devices_content_container">
+          <div className="devices_content_left">
+            <img
+              src="/devices_content_left_img.png"
+              alt=""
+              className="img-fluid"
+            />
+            <h2>Get Flax Card</h2>
+            <p>
+              Don’t have Flax Card
+              <br />
+              Purchase them here.
+            </p>
+            <a
+              href="https://caard.mini.store/"
+              target="blank"
+              className="devices-primary d-inline-block"
+            >
+              Get Card
+            </a>
+          </div>
+          <div className="devices_content_border"></div>
+          <div className="devices_content_right">
+            <img
+              src="/devices_content_right_img.png"
+              alt=""
+              className="img-fluid"
+            />
 
-              <h2>Activate Flax Card</h2>
-              <p>
-                If you already have the Flax Card
-                <br />
-                simply activate them here.
-              </p>
-              <a
-                href="https://caard.mini.store/"
-                target="blank"
-                className="devices-primary d-inline-block"
-              >
-                Activate Card
-              </a>
-            </div>
+            <h2>Activate Flax Card</h2>
+            <p>
+              If you already have the Flax Card
+              <br />
+              simply activate them here.
+            </p>
+            <a
+              href="https://caard.mini.store/"
+              target="blank"
+              className="devices-primary d-inline-block"
+            >
+              Activate Card
+            </a>
           </div>
         </div>
       </div>
