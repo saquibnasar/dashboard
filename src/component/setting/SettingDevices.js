e3aac174cdec01fcdd6098fe3a3637a8a2bb3241
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2023-06-24 15:30:12 +0530

    text changes made

--- src/component/setting/SettingDevices.js
+++ src/component/setting/SettingDevices.js
@@ -6,10 +6,12 @@ export default function SettingDevices() {
       <div className="devices">
         <h1>Activate Flax to Sunny’s profile</h1>
         <h2>
-          Scan the QR Code with your phone to launch the
-          <br className="d-md-none" /> Activation flow
+          scan the QR code to download app and launch the Card Activation
+          process
+          <br className="d-md-none" />
+          Use Flax app to Activate NFC cards
         </h2>
-        <h3>Use your Flax app to activate devices</h3>
+        <h3></h3>
         <img src="/qrcode.png" alt="" className="img-fluid" />
       </div>
     </>
